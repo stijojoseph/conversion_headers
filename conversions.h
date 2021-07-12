3a889796dee614c95b52050cc71579b3da86a0b2
stijo joseph <stijovalayil@gmail.com>
2021-07-12 16:52:06 +0530

    Add files via upload

---
+++ conversions.h
@@ -0,0 +1,32 @@
+/*
+ * conversions.h
+ *
+ *  Created on: Jul 11, 2021
+ *      Author: HP
+ */
+
+#ifndef INC_CONVERSIONS_H_
+#define INC_CONVERSIONS_H_
+
+
+
+#include <stdio.h>
+int char_to_int(char *f);
+char *int_to_char(int c);
+int string_to_hex(char *hex);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* INC_CONVERSIONS_H_ */
